2db52d8ebb37b4daf96ced8257a02abf06e47d0d
helelys <tallyslabanca20@gmail.com>
2025-03-19 21:26:36 -0300

    Finalizado e corrigido os bugs

--- src/main/java/com/login/tela/de/login/configuration/Config.java
+++ src/main/java/com/login/tela/de/login/configuration/Config.java
@@ -2,12 +2,30 @@ package com.login.tela.de.login.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class Config {
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 
+        // Adicionando usuários com suas credenciais
+        manager.createUser(User.withUsername("user")
+                .password("{noop}password") // {noop} significa que a senha é sem codificação
+                .roles("USER")
+                .build());
+
+        manager.createUser(User.withUsername("admin")
+                .password("{noop}admin")
+                .roles("USER", "ADMIN")
+                .build());
+
+        return manager;
+    }
 }
